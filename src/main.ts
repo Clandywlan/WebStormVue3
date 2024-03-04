8f6eebc3c6d22c7a5781edcd0586107927664acb
Clandywlan <1939611819@qq.com>
2024-03-04 20:11:45 +0800

    完成路由功能，准备进行登录界面的编写

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from "@/router/indexPage";
+//引入组件库
+import TDesign from 'tdesign-vue-next';
+import 'tdesign-vue-next/es/style/index.css';
+import 'tdesign-vue-next/dist/reset.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const app=createApp(App)
+app.use(router)
+app.use(TDesign)
+app.mount('#app')
 
-createApp(App).mount('#app')
